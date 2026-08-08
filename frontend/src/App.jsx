37d6144074a2aeb454df5475c35b8c9b7c7ae4bc
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-08-08 19:04:59 +0530

    Fix Login import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login/Login";
+import Login from "./pages/Login/login";
 import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard/EmployeeDashboard";
 
